107e31d01eb657abd8619d6faf8fc0a405dc4d7b
makki9532 <makki9532@gmail.com>
2022-10-23 00:44:48 +0100

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['postgres'],
+        'HOST': os.environ['todoapireq-server'],
+        'USER': os.environ['553YIZN4741E80A4$'],
+        'PASSWORD': os.environ['xqydscviat'] 
     }
 }
 
